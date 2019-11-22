1b7108ef91e8d21d10058f36542cd353594724a8
Matheus Della Croce Oliveira <matheus.comp2010@gmail.com>
2019-11-21 21:49:19 -0300

    convert to python3

--- DEMO_0.0.4.py
+++ DEMO_0.0.4.py
@@ -46,7 +46,7 @@ def main():
 				terms = tokenize(text)
 				attentive = is_attentive(terms)
 				if not attentive:
-					print(Goodbye'
+					print('Goodbye')
 					return None
 				g = update_graph(g, terms)
 				g.make_gg_plot()
@@ -75,7 +75,7 @@ def prepare_mic():
 	return r, m	
 
 def choose_dataset(g):
-	print(Type file name.'
+	print('Type file name.')
 	filename = os.getcwd()+'/'+raw_input('Filename: '+os.getcwd()+'/')
 	try:
 		dataset = pd.read_csv(filename)
@@ -96,7 +96,7 @@ def choose_dataset(g):
 	return g
 
 def get_audio(r, source):
-	print "\n Listening..."
+	print ("\n Listening...")
 	audio = r.listen(source)
 	return audio
 
